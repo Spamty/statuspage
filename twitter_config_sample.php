3af9be662757ea6e5190e03079e2c17ab57cd57e
philipp-r <philipp-r@users.noreply.github.com>
2016-07-31 15:53:31 +0200

    No sensitive data in GitHub

---
+++ twitter_config_sample.php
@@ -0,0 +1,12 @@
+<?php
+ 
+return [
+    'consumer_key'      => 'abc',
+    'consumer_secret'   => 'abc',
+    
+    'oauth_token' => '1234-abcd',
+    'oauth_token_secret' => 'abcd',
+    
+    'url_login'         => 'https://status.spamty.eu/twitter_login.php',
+    'url_callback'      => 'https://status.spamty.eu/twitter_callback.php',
+];
